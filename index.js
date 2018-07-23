ad738b2aaefa258319fa3a3778f056eeb13fa071
Tatiana Sherrill <anilorac_sol@yahoo.com>
2018-07-23 03:40:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  var newObject = Object.assign({}, object, {[key]})
+  var newObject = Object.assign({}, object)
   delete newObject.key
 }
